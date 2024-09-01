80d6feb8fbab3e990b2f7dbedbf35ba29fbb4780
Waylon Gao <124208569+WaylonGao@users.noreply.github.com>
2024-09-01 14:30:57 +0100

    basic code

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from lightkurve import search_targetpixelfile
+from lightkurve import TessTargetPixelFile
+import lightkurve as lk
+import numpy as np
 
+pixelFile = search_targetpixelfile('KIC 6922244', author="Kepler", cadence="long", quarter=4).download()
+
+pixelFile.plot(frame=42)
