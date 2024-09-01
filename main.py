20e116a9df0ce637e5eb5af17bb61bbd1f43f2d2
Waylon Gao <124208569+WaylonGao@users.noreply.github.com>
2024-09-01 14:54:10 +0100

    Plotting fixed

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from lightkurve import search_targetpixelfile
 from lightkurve import TessTargetPixelFile
 import lightkurve as lk
 import numpy as np
-
+import matplotlib.pyplot as plt
 pixelFile = search_targetpixelfile('KIC 6922244', author="Kepler", cadence="long", quarter=4).download()
 
 pixelFile.plot(frame=42)
+plt.show()
---
+++ main2.py
@@ -0,0 +1,5 @@
+from lightkurve import search_targetpixelfile
+import matplotlib.pyplot as plt
+pixelfile = search_targetpixelfile("KIC 8462852", quarter=16).download()
+pixelfile.plot(frame=1)
+plt.show()
