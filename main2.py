ec939ea19b3b489fd44b2a435c25841726eef8d9
Waylon Gao <124208569+WaylonGao@users.noreply.github.com>
2024-09-05 22:59:44 +0100

    clear repo

--- main2.py
+++
@@ -1,5 +0,0 @@
-from lightkurve import search_targetpixelfile
-import matplotlib.pyplot as plt
-pixelfile = search_targetpixelfile("KIC 8462852", quarter=16).download()
-pixelfile.plot(frame=1)
-plt.show()
